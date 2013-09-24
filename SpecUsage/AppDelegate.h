76136563cf967a285856d558c44c184143945773
Jamz Tang <me@jamztang.com>
2013-09-24 19:42:57 +0800

    Initial Commit

---
+++ SpecUsage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpecUsage
+//
+//  Created by Jamz Tang on 24/9/13.
+//  Copyright (c) 2013 Jamz Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpecUsage/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SpecUsage
+//
+//  Created by Jamz Tang on 24/9/13.
+//  Copyright (c) 2013 Jamz Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
